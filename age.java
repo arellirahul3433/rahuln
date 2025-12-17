f0ffd226c92b0f700d496a58f73ad2f1d04021b7
arellirahul3433 <arellirahul18@gmail.com>
2025-12-17 10:16:48 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,8 @@
+public class age {
+    public static void main(String[] args) {
+        int birthYear = 2004; 
+        int currentYear = 2025;
+        int age = currentYear - birthYear;
+        System.out.println("Your age is: " + age);
+    }
+}
